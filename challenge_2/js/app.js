d86de03aa0396e1732222dd0005bfc41da8c6c48
Joyce Au <jawt94@gmail.com>
2017-05-11 19:21:12 -0400

    complete challenge 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,27 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
-//
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME";
-// }
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("DetailsController", DetailsController);
+
+function WelcomeController(){
+  var vm = this;
+  vm.full_name = "JOYCE AU";
+  vm.age = 23;
+  vm.city = "Hong Kong"
+  vm.state = "Hong Kong";
+  vm.shout = function(str) {
+    return str.toUpperCase() + '!';
+  }
+}
+
+function DetailsController() {
+  var vm = this;
+  vm.class_name = "Self taught";
+  vm.enrolled_students = 1;
+  vm.start_date = "5/11/2017";
+  vm.end_date = "5/12/2017";
+  vm.daysRemaining = function(sd, ed) {
+    return Math.floor((Date.parse(ed) - Date.parse(sd))/86400000);
+  }
+}
